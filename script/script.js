a9e0f7a5dbc9dc4d07266cd1e20c0ec6b4a91b96
Aditya <aadityasp@gmail.com>
2023-03-17 12:45:33 -0400

    added ripple effect

--- script/script.js
+++ script/script.js
@@ -17,3 +17,26 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+//Adding ripple effect on mouse move.
+document.addEventListener('mousemove', function (event) {
+    // Create a ripple element
+    const ripple = document.createElement('div');
+    ripple.classList.add('ripple');
+  
+    // Set ripple element's size and position
+    const diameter = 50; // You can change this value to adjust the ripple size
+    ripple.style.width = `${diameter}px`;
+    ripple.style.height = `${diameter}px`;
+    ripple.style.left = `${event.clientX - diameter / 2}px`;
+    ripple.style.top = `${event.clientY - diameter / 2}px`;
+  
+    // Add the ripple element to the DOM
+    document.body.appendChild(ripple);
+  
+    // Remove the ripple element after the animation ends
+    setTimeout(() => {
+      ripple.remove();
+    }, 1000);
+  });
+  
