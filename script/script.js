e613e94908bf35d85d4bf4ccb71740c2d592fa0f
Aditya <aadityasp@gmail.com>
2023-03-17 13:01:49 -0400

    modified js to look like code

--- script/script.js
+++ script/script.js
@@ -38,25 +38,57 @@ navLinks.forEach((l) => {
 //   });
 
 //MAtrix code effect
-const matrixChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-const matrixLength = matrixChars.length;
+// const matrixChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+// const matrixLength = matrixChars.length;
 
-function randomMatrixChar() {
-  return matrixChars[Math.floor(Math.random() * matrixLength)];
-}
+// function randomMatrixChar() {
+//   return matrixChars[Math.floor(Math.random() * matrixLength)];
+// }
 
-document.addEventListener('mousemove', function (event) {
-  const matrixCode = document.createElement('span');
-  matrixCode.classList.add('matrix-code');
-  matrixCode.innerText = randomMatrixChar();
-  matrixCode.style.left = `${event.clientX}px`;
-  matrixCode.style.top = `${event.clientY}px`;
+// document.addEventListener('mousemove', function (event) {
+//   const matrixCode = document.createElement('span');
+//   matrixCode.classList.add('matrix-code');
+//   matrixCode.innerText = randomMatrixChar();
+//   matrixCode.style.left = `${event.clientX}px`;
+//   matrixCode.style.top = `${event.clientY}px`;
 
-  document.body.appendChild(matrixCode);
+//   document.body.appendChild(matrixCode);
 
-  setTimeout(() => {
-    matrixCode.remove();
-  }, 800);
-});
-
+//   setTimeout(() => {
+//     matrixCode.remove();
+//   }, 800);
+// });
+const codeSamples = [
+    "int main() {",
+    "  std::cout << 'Hello, World!' << std::endl;",
+    "  return 0;",
+    "}",
+    "def hello():",
+    "  print('Hello, World!')",
+    "hello()",
+    "class MyClass:",
+    "  def __init__(self):",
+    "    self.data = 42",
+    "  def display(self):",
+    "    print(self.data)"
+  ];
+  
+  function randomCodeLine() {
+    return codeSamples[Math.floor(Math.random() * codeSamples.length)];
+  }
+  
+  document.addEventListener("mousemove", function (event) {
+    const codeLine = document.createElement("span");
+    codeLine.classList.add("matrix-code");
+    codeLine.innerText = randomCodeLine();
+    codeLine.style.left = `${event.clientX}px`;
+    codeLine.style.top = `${event.clientY}px`;
+  
+    document.body.appendChild(codeLine);
+  
+    setTimeout(() => {
+      codeLine.remove();
+    }, 800);
+  });
+  
   
