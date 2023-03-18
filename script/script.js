eeb3643fef72d5c6e96b8739f20886f32e3bc7d6
Aditya <aadityasp@gmail.com>
2023-03-18 11:51:24 -0400

    added contact form

--- script/script.js
+++ script/script.js
@@ -18,46 +18,39 @@ navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
 
-// //Adding ripple effect on mouse move.
-// document.addEventListener('mousemove', function (event) {
-//     // Create a ripple element
-//     const ripple = document.createElement('div');
-//     ripple.classList.add('ripple');
+//emailjs
+// Replace 'your_user_id' with your EmailJS User ID
+emailjs.init('ghT20476Zcl43zo1U');
+
+//code to handle form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
   
-//     // Set ripple element's position
-//     ripple.style.left = `${event.clientX}px`;
-//     ripple.style.top = `${event.clientY}px`;
+    // Replace these values with your Service ID and Template ID
+    const serviceID = 'service_yvg59sy';
+    const templateID = 'template_9boqmz2';
   
-//     // Add the ripple element to the DOM
-//     document.body.appendChild(ripple);
+    // Get form data
+    const fromName = document.querySelector('input[name="name"]').value;
+    const fromEmail = document.querySelector('input[name="email"]').value;
+    const subject = document.querySelector('input[name="subject"]').value;
+    const message = document.querySelector('textarea[name="message"]').value;
   
-//     // Remove the ripple element after the animation ends
-//     setTimeout(() => {
-//       ripple.remove();
-//     }, 2000);
-//   });
+    // Send the email using EmailJS
+    emailjs.send(serviceID, templateID, {
+      from_name: fromName,
+      from_email: fromEmail,
+      subject: subject,
+      message: message,
+    })
+    .then(function(response) {
+      alert('Your message has been sent!');
+    }, function(error) {
+      alert('Failed to send the message. ' + error);
+    });
+  });
+  
 
-//MAtrix code effect
-// const matrixChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-// const matrixLength = matrixChars.length;
-
-// function randomMatrixChar() {
-//   return matrixChars[Math.floor(Math.random() * matrixLength)];
-// }
-
-// document.addEventListener('mousemove', function (event) {
-//   const matrixCode = document.createElement('span');
-//   matrixCode.classList.add('matrix-code');
-//   matrixCode.innerText = randomMatrixChar();
-//   matrixCode.style.left = `${event.clientX}px`;
-//   matrixCode.style.top = `${event.clientY}px`;
-
-//   document.body.appendChild(matrixCode);
-
-//   setTimeout(() => {
-//     matrixCode.remove();
-//   }, 800);
-// });
 const codeSamples = [
     "int main() {",
     "  std::cout << 'Hello, World!' << std::endl;",
